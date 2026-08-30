3ecc91a74987bc43b79745db383fec6e9fb48789
Pavlo <pavelfialkovsky@gmail.com>
2026-08-30 01:59:37 -0400

    Automatically create notes table when missing

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_db_connection():
     return conn
 
 def init_db():
-    conn = get_db_connection
+    conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS notes(id SERIAL PRIMARY KEY, content TEXT NOT NULL);
     ''')
